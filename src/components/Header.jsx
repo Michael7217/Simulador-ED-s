2d5708d4b84f75a19323c03213c37cc19919e989
Gabriel01Gilvan <gabrielgilvan757@gmail.com>
2026-04-03 16:58:49 -0300

    Adiciona botão de simulação no Header (visível apenas nas estruturas) e unifica o código das páginas de conceito (textos pendentes).

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 import menuicon from '../assets/MenuIcon.png'
 import Logo from '../assets/Logo.png'
-
+import { useLocation, useNavigate } from 'react-router-dom'
 
 
 export default function Header(props){
-    
+    const pagina = useLocation();
+    const navigate = useNavigate();
+    const pathAtual = pagina.pathname.split('/').pop();
+
+    let botao=false;
+    if(pagina.pathname !== '/'){
+        botao = true
+    }
+
     return(
-        <header className="relative z-10 w-full h-18 bg-azul flex items-center px-4 rounded-b-xl">
-            <div className="h-5 w-8" onClick={()=>{props.funcao()}} >
+        <header className="relative z-10 w-full h-18 bg-azul flex items-center px-4 rounded-b-xl justify-between">
+            <div className="h-5 w-8" onClick={()=>{props.funcao()}}>
                 <img src={menuicon} alt="menu" className="cursor-pointer"/>
             </div>
             <div className="h-auto w-full flex items-center justify-center gap-2 text-2xl">
                 <img src={Logo} alt="logo" />
-                <h1 className="font-bold">Simulador ED`s</h1>
+                <h1 className="font-bold text-branco">Simulador ED`s</h1>
             </div>
+            <div className="w-24 md:w-32 flex justify-end">
+                {botao && (
+                    <a onClick={() => navigate(`/simulacao/${pathAtual}`)} className="text-amarelo font-bold text-sm md:text-lg hover:!text-laranja transition-all duration-300 cursor-pointer">Simulação</a>
+                )}
+                </div>
 
         </header>
     )
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,19 @@
 import {Routes, Route} from 'react-router-dom'
 import Home from '../pages/Home'
 import Layout from '../layout/Layout'
+import PaginaEstrutura from '../pages/PaginaEstrutura'
 
 export default function AppRoutes(){
     return(
         <Routes>
             <Route path='/' element={<Layout/>}>
                 <Route index element={<Home/>}/>
+                <Route path='/lista-encadeada' element={<PaginaEstrutura index={0} />}/>
+                <Route path='/fila' element={<PaginaEstrutura index={1} />}/>
+                <Route path='/arvore-binaria' element={<PaginaEstrutura index={2} />}/>
+                <Route path='/pilha' element={<PaginaEstrutura index={3} />}/>
+                <Route path='/arvore-avl' element={<PaginaEstrutura index={4} />}/>
+                <Route path='/arvore' element={<PaginaEstrutura index={5} />}/>
             </Route>
         </Routes>
     )
